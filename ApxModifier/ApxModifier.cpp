79e853330a2c7213cabdb1e7900582adae66e6d2
Sangwoo Ham <flyhamsw@gmail.com>
2017-02-10 10:54:33 +0900

    Revert "Roll Pitch Heading(Yaw)"
    This reverts commit 1eb07fadc61dbfd4114dd79185f60c157a4fa8d0.

--- ApxModifier/ApxModifier.cpp
+++ ApxModifier/ApxModifier.cpp
@@ -357,7 +357,7 @@ void ApxModifier::writeNewFile(char* txt_filename)
 
 		if (fnew.is_open())
 		{
-			fnew << jpgName << ".jpg" << '\t' << rowInterpolated->X << '\t' << rowInterpolated->Y << '\t' << rowInterpolated->Z << '\t' << rowInterpolated->roll << '\t' << rowInterpolated->pitch << '\t' << rowInterpolated->heading;
+			fnew << jpgName << ".jpg" << '\t' << rowInterpolated->X << '\t' << rowInterpolated->Y << '\t' << rowInterpolated->Z << '\t' << rowInterpolated->heading << '\t' << rowInterpolated->roll << '\t' << rowInterpolated->pitch;
 		}
 
 		fnew.close();

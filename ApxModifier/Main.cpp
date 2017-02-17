d2e71406b482a0c28fcc54a62442462bcc2e9e67
Sangwoo Ham <flyhamsw@gmail.com>
2017-02-17 14:59:48 +0900

    main

--- ApxModifier/Main.cpp
+++ ApxModifier/Main.cpp
@@ -6,10 +6,11 @@
 
 int main()
 {
+
 	char* filename; //기존 파일명
 
 	//기존 파일 로드
-	filename = "D://APXTEST/Original/2017-02-07_145101_apx.txt";
+	filename = "D://APXTEST/Original/2017-02-07_155207_apx.txt";
 	ApxModifier* a = new ApxModifier(filename, true);
 
 	//이벤트 전, 후 데이터 검색 및 계산
@@ -18,7 +19,21 @@ int main()
 	//새 파일 생성
 	char newFilename[200] = "D://APXTEST/test.txt";
 	a->writeNewFile(newFilename);
-	//>printLocationData(newFilename);
+	
+	
+	/*
+	//QGIS 가시화용 코드
+	
+	char* filename;
+	filename = "D://APXTEST/Original/2017-02-07_155227_apx.txt";
+	ApxModifier* a = new ApxModifier(filename, true);
+
+	a->interpolateData();
+	
+	//새 파일 생성
+	char newFilename[200] = "D://APXTEST/2017-02-07_155227_apx.txt";
+	a->printLocationData(newFilename);
+	*/
 
 	return 0;
 }

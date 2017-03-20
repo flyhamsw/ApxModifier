eb12f43ae27ec566ac0762d82a08b484bd3f5ce2
Sangwoo Ham <flyhamsw@gmail.com>
2017-03-20 21:55:39 +0900

    test

--- ApxModifier/Main.cpp
+++ ApxModifier/Main.cpp
@@ -10,14 +10,14 @@ int main()
 	char* filename; //기존 파일명
 
 	//기존 파일 로드
-	filename = "D://test/Original/2017-02-13_151557_apx.txt";
+	filename = "D:/data_wgs84/original/2017-02-13_151445_apx.txt";
 	ApxModifier* a = new ApxModifier(filename, false);
 
 	//이벤트 전, 후 데이터 검색 및 계산
 	a->interpolateData();
 
 	//새 파일 생성
-	char newFilename[200] = "D://test/2017-02-13_151557.txt";
+	char newFilename[200] = "D://data_wgs84/test-tm.txt";
 	a->writeNewFile(newFilename);
 	
 	
